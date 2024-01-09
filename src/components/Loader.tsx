613d56e9d4d339b485cfe284c811ff6ea821db59
akash-crownstack <akash@crownstack.com>
2024-01-09 18:14:57 +0530

    changed loader color

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -6,7 +6,7 @@ interface LoaderProps {
 const Loader = ({ width, color }: LoaderProps) => {
   return (
     <div
-      className={`${width} h-6 rounded-full animate-spin border-2 border-solid border-${color} border-t-transparent`}
+      className={`${width} h-6 rounded-full animate-spin border-2 border-solid ${color} border-t-transparent`}
     ></div>
   );
 };
