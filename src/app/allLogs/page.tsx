e2e7d0496a625c8c79dc84d4581c3041f1ee58b3
Madhur1893 <madhur.verma@crownstack.com>
2024-01-11 14:57:09 +0530

    Created change-log page ui

--- src/app/allLogs/page.tsx
+++ src/app/allLogs/page.tsx
@@ -7,6 +7,8 @@ import { db } from "@/lib/db";
 import dayjs from "dayjs";
 import Link from "next/link";
 import React from "react";
+import ChangeLogCard from "@/components/ChangeLogCard";
+import ChangeLogDetail from "@/components/ChangeLogDetail";
 
 const getPosts = async () => {
   const response = await db.logs.findMany({
@@ -31,20 +33,20 @@ export default async function AllLogs({ req }: any) {
   return (
     <>
       <Navbar />
-      <MaxWidthWrapper>
-        <div className="md:flex md:items-center md:justify-between py-4 px-6">
-          <TypographyH3 className="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">
-            Change Logs
-          </TypographyH3>
-          <Link
-            href="/changeLog/add"
-            className="ml-3 inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-          >
-            Add New
-          </Link>
-        </div>
-        <main className="grid items-center justify-center md:grid-cols-4 lg:grid-cols-4 gap-4 mt-10">
-          {changeLogs.map((logs) => (
+
+      <div className="md:flex md:items-center md:justify-between py-4 px-6">
+        <TypographyH3 className="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">
+          Change Logs
+        </TypographyH3>
+        <Link
+          href="/changeLog/add"
+          className="ml-3 inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+        >
+          Add New
+        </Link>
+      </div>
+      <main className="border-t border-slate-300 grid justify-center md:grid-cols-3 lg:grid-cols-3 sm:grid-cols-3 xs:grid-cols-3">
+        {/* {changeLogs.map((logs) => (
             <Link href={`/changeLog/${logs.log_id}`} key={logs.log_id}>
               <Card className="px-4 py-4 shadow-md border-2">
                 <TypographyH3>{logs.title}</TypographyH3>
@@ -53,9 +55,14 @@ export default async function AllLogs({ req }: any) {
                 </TypographyP>
               </Card>
             </Link>
-          ))}
-        </main>
-      </MaxWidthWrapper>
+          ))} */}
+        <div className="col-span-1 bg-gray-100 border border-t-0">
+          <ChangeLogCard changeLogs={changeLogs} />
+        </div>
+        <div className="col-span-2 bg-gray-100">
+          <ChangeLogDetail />
+        </div>
+      </main>
     </>
   );
 }
