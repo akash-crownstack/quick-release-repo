ede8be6623701e931e431e7951675e29ef15edca
akash-crownstack <akash@crownstack.com>
2024-02-14 16:53:21 +0530

    fix input type at reset password

--- src/app/reset-password/[token]/page.tsx
+++ src/app/reset-password/[token]/page.tsx
@@ -108,7 +108,7 @@ const ResetPassword = ({ params }: any) => {
                 New Password
               </label>{" "}
               <input
-                type="email"
+                type="password"
                 {...register("password")}
                 id="email"
                 className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
